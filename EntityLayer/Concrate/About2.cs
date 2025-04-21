62409f1e8cb0d3c66cb24d0547071d73a1c702f8
Amir AFSHAR <152279280+amirafshar2@users.noreply.github.com>
2025-04-21 11:30:46 +0200

    Add project files.

---
+++ EntityLayer/Concrate/About2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class About2
+    {
+        public int Aboute2ID { get; set; }
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Descriotion { get; set; }
+        public string Image { get; set; }
+
+    }
+}
---
+++ EntityLayer/Concrate/Guide.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrate
+{
+    public class Guide
+    {
+        public int GuidID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string  TwiterUrl { get; set; }
+        public string InstgramUrl { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
